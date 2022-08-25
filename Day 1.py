baa948c0be5bbed8f9afe7d11d4b92d49cd0fda7
Binh <ntbinh37engineer@gmail.com>
2022-08-25 18:43:12 +0700

    Add_day 1

---
+++ Day 1.py
@@ -0,0 +1,24 @@
+# print("HEllo my name is Binh nguyen")
+# print("Hello my name"+" is Binh Nguyen")
+#1.2 Excercise Fix the code below
+# print("Day1 - String manipulation")
+# print("String Concatenation is done with the"+" sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+#input
+# print("Hello " + input("What is your name? ") + "!")
+# 1.3 input Excercise
+# print(len(input("What is your name? ")))
+#1.4 Variable Excercise
+# a = input("a:")
+# b = input("b:")
+# c = a
+# a = b
+# b = c
+# print("a:"+a)
+# print("b:"+b)
+#Final Excercise
+print("Welcome to the Band name generator")
+name = input("Which city did you grow up in? ")
+pet = input("Which name of the pet did you grow up in? ")
+print("Your band name is: "+name+" "+pet)
